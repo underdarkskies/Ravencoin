e9aa27206e41ed41c2d403f1f185321ed82fe932
blondfrogs <jeanderson@MacBook-Pro-3.local>
2018-11-06 14:00:04 -0700

    Make units default to 0 on create asset in the GUI

--- src/qt/createassetdialog.cpp
+++ src/qt/createassetdialog.cpp
@@ -217,7 +217,7 @@ CreateAssetDialog::~CreateAssetDialog()
 /** Helper Methods */
 void CreateAssetDialog::setUpValues()
 {
-    ui->unitBox->setValue(8);
+    ui->unitBox->setValue(0);
     ui->reissuableBox->setCheckState(Qt::CheckState::Checked);
     ui->ipfsText->hide();
     hideMessage();
@@ -1113,7 +1113,7 @@ void CreateAssetDialog::clearSelected()
     ui->unitBox->setDisabled(false);
     ui->quantitySpinBox->setDisabled(false);
     ui->reissuableBox->setChecked(true);
-    ui->unitBox->setValue(8);
+    ui->unitBox->setValue(0);
 }
 
 void CreateAssetDialog::updateAssetList()
